22edaa88c7b9f41b9d6c55b7ad68deb5d829ccb7
Daiki Fujimori <G016C1102@outlook.jp>
2025-06-01 15:09:20 +0900

    Initial Commit

---
+++ TCAInspiredVIPER/TCAInspiredVIPERApp.swift
@@ -0,0 +1,18 @@
+//
+//  TCAInspiredVIPERApp.swift
+//  TCAInspiredVIPER
+//  
+//  Created by Daiki Fujimori on 2025/06/01
+//  
+
+
+import SwiftUI
+
+@main
+struct TCAInspiredVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCAInspiredVIPERTests/TCAInspiredVIPERTests.swift
@@ -0,0 +1,18 @@
+//
+//  TCAInspiredVIPERTests.swift
+//  TCAInspiredVIPERTests
+//  
+//  Created by Daiki Fujimori on 2025/06/01
+//  
+
+
+import Testing
+@testable import TCAInspiredVIPER
+
+struct TCAInspiredVIPERTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
